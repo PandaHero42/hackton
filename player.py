3dee55bd97c0079e8398f4a528b24fe87b7665c0
CoralieQUE <coralie.quenon@std.heh.be>
2025-05-16 12:49:49 +0200

    feat: add player

---
+++ player.py
@@ -0,0 +1,10 @@
+class Player:
+    def __init__(self, name, score=0):
+        self.name = name
+        self.score = score
+
+    def add_score(self, points):
+        self.score += points
+
+    def __str__(self):
+        return f"{self.name}: {self.score}"
